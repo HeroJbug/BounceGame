243c6ec9d49b5c9406e7e3e32a438422787a53ab
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-09-23 20:32:19 -0600

    Basic Movement

---
+++ Bounce/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(speed*Input.GetAxis("Horizontal"), speed*Input.GetAxis("Vertical"), 0));
+    }
+}
