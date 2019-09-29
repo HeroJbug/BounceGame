e14fe28e5745ee810cbe88206c44a6f26e9fb3b6
Fluteboy123 <u1032555@umail.utah.edu>
2019-09-29 14:58:25 -0600

    Enemies explode when they hit the wall

---
+++ Bounce/Assets/Scripts/Hazards/Walls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walls : MonoBehaviour
+{
+    public ParticleSystem explosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Vector3 hitPoint = collision.gameObject.transform.position;
+            Destroy(collision.gameObject);
+            Instantiate(explosion, hitPoint,Quaternion.identity);
+        }
+    }
+}
