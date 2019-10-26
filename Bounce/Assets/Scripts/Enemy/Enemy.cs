2be723401e136c51c9492fb8f9e73140ca3bfd31
Garrett <gmoseke@gmail.com>
2019-10-26 15:54:23 -0600

    More path smoothing stuff.
    still a touch jittery

--- Bounce/Assets/Scripts/Enemy/Enemy.cs
+++ Bounce/Assets/Scripts/Enemy/Enemy.cs
@@ -67,38 +67,28 @@ public class Enemy : MonoBehaviour
         transform.right = target.position - transform.position;
         while (followingPath)
         {
-            if (transform.position == currentWP)
+            //SMOOTHED CALCULATION
+            Vector2 pos = new Vector2(transform.position.x, transform.position.y);
+            if (path.turnBoundaries[pathIdx].HasCrossedLine(pos))
             {
-                targetIdx++;
-                if (targetIdx >= path.lookPoints.Length)
+                if (pathIdx == path.finishLineIdx)
                 {
-                    yield break;
+                    followingPath = false;
                 }
-                currentWP = path.lookPoints[targetIdx];
+                else
+                {
+                    pathIdx++;
+                }
             }
 
-            transform.position = Vector3.MoveTowards(transform.position, currentWP, speed * Time.deltaTime);
-
-            //SMMOTHED CALCULATION
-            //Vector2 pos = new Vector2(transform.position.x, transform.position.y);
-            //if(path.turnBoundaries[pathIdx].HasCrossedLine(pos))
-            //{
-            //    if(pathIdx == path.finishLineIdx)
-            //    {
-            //        followingPath = false;
-            //    }
-            //    else
-            //    {
-            //        pathIdx++;
-            //    }
-            //}
-
-            //if(followingPath)
-            //{
-            //    Quaternion targetRot = Quaternion.LookRotation(path.lookPoints[pathIdx] - transform.position);
-            //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, Time.deltaTime * turnSpeed);
-            //    transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.Self);
-            //}
+            if (followingPath)
+            {
+                float angle =Mathf.Atan2(path.lookPoints[pathIdx].y - transform.position.y, path.lookPoints[pathIdx].x - transform.position.x);
+                angle *= Mathf.Rad2Deg;
+                Quaternion targetRot = Quaternion.Euler(0f, 0f, angle);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, Time.deltaTime * turnSpeed);
+                transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self);
+            }
             yield return null;
 
         }
