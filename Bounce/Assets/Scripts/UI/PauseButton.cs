cbc5b4253179091b3b5dfdb27ae8f78f31ab9cf7
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-10-28 18:18:23 -0600

    Add the ability to pause

---
+++ Bounce/Assets/Scripts/UI/PauseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+    public Image pauseScreen;
+    private static bool paused = false;
+    private Image currentScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseScreen.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                pauseScreen.enabled = true;
+                Time.timeScale = 0f;
+                paused = true;
+            }
+            else
+            {
+                pauseScreen.enabled = false;
+                Time.timeScale = 1f;
+                paused = false;
+            }
+        }
+    }
+}
