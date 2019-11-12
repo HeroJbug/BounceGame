82af521cfb22eb4a5e2c5049b79b537c12ff1d2a
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-11-11 19:31:20 -0700

    Edited starting animation

--- Bounce/Assets/Scripts/Animations/P67_Start.cs
+++ Bounce/Assets/Scripts/Animations/P67_Start.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class P67_Start : MonoBehaviour
 {
     Rigidbody2D p67Body;
-    public float startSpeed,bounceBack,finalDrag;
-    public Sprite finalSprite;
+    public float startSpeed,bounceBack,boostDelay,boostScale,finalDrag;
+    public Sprite[] sprites;
     void Start()
     {
         p67Body = GetComponent<Rigidbody2D>();
@@ -16,12 +16,21 @@ public class P67_Start : MonoBehaviour
     public void MoveRight()
     {
         p67Body.AddRelativeForce(startSpeed * new Vector2(1, 0), ForceMode2D.Impulse);
+        GetComponent<Image>().sprite = sprites[0];
+        transform.localScale = new Vector3(boostScale, boostScale, boostScale);
+        Invoke("BigBoost", boostDelay);
+    }
+
+    public void BigBoost()
+    {
+        GetComponent<Image>().sprite = sprites[1];
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         p67Body.AddRelativeForce(bounceBack * new Vector2(-1, 0), ForceMode2D.Impulse);
-        GetComponent<Image>().sprite = finalSprite;
+        GetComponent<Image>().sprite = sprites[2];
+        transform.localScale = new Vector3(1,1,1);
         p67Body.drag = finalDrag;
     }
 }
