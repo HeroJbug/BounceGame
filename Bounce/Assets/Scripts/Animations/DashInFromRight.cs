04b46ad7523ee53186a7e30e60a7075a43887be8
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-11-11 19:59:12 -0700

    Created a button animation

---
+++ Bounce/Assets/Scripts/Animations/DashInFromRight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashInFromRight : MonoBehaviour
+{
+    public float duration,startX,endX;
+    bool finished = true;
+    float timePassed = 0f;
+
+    private void Start()
+    {
+        transform.position = new Vector3(startX, transform.position.y, transform.position.z);
+        Invoke("StartTimer", 0.5f);
+    }
+
+    public void StartTimer()
+    {
+        finished = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!finished)
+            timePassed += Time.deltaTime;
+        if (timePassed >= duration)
+        {
+            finished = true;
+            transform.position = new Vector3(endX, transform.position.y, transform.position.z);
+        }
+        if(!finished)
+            transform.position = new Vector3(endX + ((startX-endX)*(timePassed-duration)*(timePassed-duration))/(duration*duration), transform.position.y, transform.position.z);
+    }
+}
